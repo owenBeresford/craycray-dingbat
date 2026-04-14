a6444d8daf0639bb7fba6854f18e78be564a8a66
oab1 <oab1@users.sourceforge.net>
2026-04-14 16:10:07 +0100

    Add more checks in unit-test; Storybook objects to Regex with g flag;

--- client-src/src/test/storybook-suspence/TabBar.stories.ts
+++ client-src/src/test/storybook-suspence/TabBar.stories.ts
@@ -33,7 +33,7 @@ export const TrackTextRendered: Story = {
     const canvas = within(canvasElement);
     // I am defining this, as some browsers are damaging/ transliterating my UTF8 chars,
     // so the tests fail in an annoying fashion.
-    const EXTRACT_NON_ASCII: RegExp = /[^\x00-\x7F]+/gu;
+    const EXTRACT_NON_ASCII: RegExp = /[^\x00-\x7F]+/u;
 
     // might need to add .resolves. to expect statements
     expect(canvas.queryByTestId("test15")).toBeVisible();
@@ -59,7 +59,7 @@ export const TrackTextRendered8: Story = {
     const canvas = within(canvasElement);
     // I am defining this, as some browsers are damaging/ transliterating my UTF8 chars,
     // so the tests fail in an annoying fashion.
-    const EXTRACT_NON_ASCII: RegExp = /[^\x00-\x7F]+/gu;
+    const EXTRACT_NON_ASCII: RegExp = /[^\x00-\x7F]+/u;
 
     // might need to add .resolves. to expect statements
     expect(canvas.queryByTestId("test24")).toBeVisible();
@@ -72,7 +72,7 @@ export const TrackTextRendered8: Story = {
     await step('list all lists', async () => {
         expect(await canvas.findByText("List All")).toBeVisible();
         await userEvent.click(canvas.getByText("List All"));
-        // Story may break here
+        // Story may break here as should shift to another URL
     });
 
     await step('create new list', async () => {
@@ -90,7 +90,7 @@ export const TrackTextRendered8: Story = {
         await userEvent.click(canvas.getByText("Show help"));
         expect(STORE.state.showHelp).toBe(true);
    //     expect( await canvas.findByTestId("XXX")).toBeVisible();
-   // interstitial not in navbar itself, so cannot trigger anything just here 
+   // interstitial is not in navbar itself, so cannot trigger anything just here 
     });
     
     await step('attempt installation', async () => {
@@ -112,13 +112,18 @@ export const TrackTextRendered8: Story = {
         expect(await canvas.findByText("Rename list")).toBeVisible();
         const NEWDATA= createDataFactory(fixture2());
         if(!NEWDATA.currentData) { throw new Error("No data found after fixture loaded??"); }
-      
+        ListData.updateData( NEWDATA.currentData);
+        const STORE=useStore(); 
+        STORE.commit('setId', 1);
+
         const liste1 = NEWDATA.currentData.get(1);
         expect( liste1?.nom).toBe( "list 1");
-     //   let image=liste1?.nom;
         await userEvent.click(canvas.getByText("Rename list"));
+        expect( await canvas.findByTestId("test24input1")).toBeVisible();
+        await userEvent.type( await canvas.findByLabelText("Enter your new value: ", { selector: "input" }), "3333" );
+        await userEvent.click(canvas.getByText("Set"));
         expect( liste1?.nom).not.toBe( "list 1");
-   //     expect( await canvas.findByTestId("XXX")).toBeVisible();
+   
      });
 
     await step('Duplicate list', async () => {
@@ -136,7 +141,7 @@ export const TrackTextRendered8: Story = {
         const NEWDATA= createDataFactory(fixture5());
         const STORE=useStore(); 
         if(!NEWDATA.currentData) { throw new Error("No data found after fixture loaded??"); }
-        STORE.setId(1);
+        STORE.commit('setId', 1);
 
         let before=NEWDATA.currentData.get(1)?.export();
         await userEvent.click(canvas.getByText("Make unique"));
@@ -146,7 +151,8 @@ export const TrackTextRendered8: Story = {
 
    await step('Save all', async () => {
         expect(await canvas.findByText("Save all")).toBeVisible();  
-        // TODO: need to extend this one 
+        // TODO: need to extend this one.
+        //       and have the server running :-)
    });
 
    await step('Revert all', async () => {
