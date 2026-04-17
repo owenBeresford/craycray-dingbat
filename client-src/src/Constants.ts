835f4f4542605856d02f571381a3db5c74467dfc
oab1 <oab1@users.sourceforge.net>
2026-04-17 13:51:57 +0100

    Improve global names to more recent nomenclatura; Add noise changes

--- client-src/src/Constants.ts
+++ client-src/src/Constants.ts
@@ -9,15 +9,14 @@ export const REMOTE_HOST = location.hostname + ":" + location.port;
 export const INSTALLED = "local-shopping";
 export const API_RETRY = 500;
 export const ROOT_NODE = "#app";
-// IOIO XXX add a real value to this.    try server ip or msgQueue channel name
-export const MSG_DESTINATION = "panic";
 
 // used in MessageDistribution
 export const PMQUE_TIMER = 300;
 export const PMQUE_ATTEMPTS = 10;
 
-// IOIO check base dir for conmpiled artefacts
+// IOIO check base dir for compiled artefacts
 export const MSG_THREAD = /* @vite-ignore */ new URL("worker1.min.mjs", import.meta.url);
+export const LOGO_PATH= "/logo.png";
 
 // values for MotionStream
 export const MOBILE_THRESHOLD = 150;
@@ -31,7 +30,9 @@ export const KNOWN_PHONE = "shopping-known";
 export const FETCH_TIMEOUT = 500; // ms
 export const DELAY_FOR_API = 500; // ms
 
-if (typeof global.fetch === "undefined" || !global.fetch) {
+
+
+if (typeof globalThis.fetch === "undefined" || !globalThis.fetch) {
   throw new Error("73453894563453 Fetch() not found.  BAILING OUT");
 }
 
@@ -45,10 +46,10 @@ if (typeof global.fetch === "undefined" || !global.fetch) {
  * @returns {RemoteStorage}
  */
 export function createRemoteService(loc: Location | WorkerLocation): RemoteStorage {
-  if (process.env.NODE_ENV !== "TEST" && (!global || !global.addEventListener)) {
+  if (process.env.NODE_ENV !== "TEST" && (!globalThis || !globalThis.addEventListener)) {
     throw new Error("8674564632343 Message passing is only possible inside a reasonable browser.");
   }
-  if (process.env.NODE_ENV !== "TEST" && (!global || (!global.Worker && !("worker_threads" in global)))) {
+  if (process.env.NODE_ENV !== "TEST" && (!globalThis || (!globalThis.Worker && !("worker_threads" in globalThis)))) {
     throw new Error("9757353545757 Message passing is only possible inside a reasonable browser.");
   }
   let d3: RemoteConfig = {
