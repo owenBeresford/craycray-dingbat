22bdcaa8e3a72e1a4d733c168e8033184bbf916e
oab1 <oab1@users.sourceforge.net>
2026-05-08 16:01:13 +0100

    Improve test, and migrate to 'new' cURL (leSigh)

--- server-src/src/test/apimodule.vitest.ts
+++ server-src/src/test/apimodule.vitest.ts
@@ -1,23 +1,17 @@
 import { assert, describe, it, expect, assertType, beforeAll, afterAll } from "vitest";
-
 // https://scribe.rip/@azizzouaghia/setting-up-basic-api-testing-with-supertest-cucumber-jest-and-typescript-8c6a23c045a1
-import { GoneException } from "@nestjs/common/exceptions";
-import { INestApplication } from "@nestjs/common";
-import { Test } from "@nestjs/testing";
-
-import { delay, runFetch } from "../../../common/util";
+ 
+import { delay, runFetch, runExecProcessOnUrl} from "../../../common/util";
 import { ShoppingBE } from "../shopping/ShoppingBE";
 import { ShoppingService } from "../shopping/ShoppingService";
 import { SaveStruct } from "../../../common/types/SaveStruct";
-// import type { TestDataSchema } from "../../client-src/src/types/ListCollection";
-// import type { PromiseSucceed, PromiseReject } from "../../../common/types/promises";
-import { fixture1, fixture2, transform2SaveStruct } from "../../../common/fixture-lists";
+import { fixture1, fixture2, fixture3, transform2SaveStruct } from "../../../common/fixture-lists";
 
 describe("I can use API module", () => {
   const TARGET: string = "https://app.hiss:3001/api/shared-state";
 
   it("can GET the API", async () => {
-    const res: SimpleResponse = runFetch(TARGET, true, undefined);
+    const res: SimpleResponse = await runExecProcessOnUrl(TARGET, undefined);
     expect(res.ok);
     expect(res.status).toBe(200);
     expect(res.headers.get("Content-Type")).toMatch(/json/);
@@ -27,7 +21,11 @@ describe("I can use API module", () => {
 
   it("can POST the API", async () => {
     // made data from fixture...
-    let res: SimpleResponse = await runFetch(TARGET, true, { method: "post", body: transform2SaveStruct(fixture1()) });
+    let res: SimpleResponse = await runExecProcessOnUrl(TARGET, 
+        { method: "POST", 
+          body: JSON.stringify(transform2SaveStruct(fixture1())), 
+          headers:{'Content-Type': 'application/json; charset=utf-8'} }
+        );
     expect(res.ok);
     expect(res.status).toBe(201);
     expect(res.headers.get("Content-Type")).toMatch(/json/);
@@ -42,52 +40,62 @@ describe("I can use API module", () => {
 
   it("more complex API session [1]", async () => {
     // made data from fixture...
-    let res: SimpleResponse = await runFetch(TARGET, true, { method: "post", body: transform2SaveStruct(fixture1()) });
+    let res: SimpleResponse = await runExecProcessOnUrl(TARGET, { 
+      method: "POST", 
+      body: JSON.stringify(transform2SaveStruct(fixture1())) ,
+      headers:{'Content-Type': 'application/json; charset=utf-8', 'Accept':'application/json; charset=utf-8'} 
+    });
     expect(res.ok);
     expect(res.status).toBe(201);
     expect(res.headers.get("Content-Type")).toMatch(/json/);
     try {
-      if (typeof res.body === "string") {
+      if (typeof res.body != "string") {
         throw Error("Expected JSON object");
       } else {
         let obj = JSON.parse(new String(res.body).trim());
         expect(obj.statusCode).toBe(204);
       }
-    } catch (e) {
-      console.log("Test quit early saying " + (e as Error).message);
+    } catch (e:unknown) {
+      console.log("[FIRST] Test quit early saying " + (e as Error).message);
       expect(false).toBe(true);
     }
 
-    ret: SimpleResponse = runFetch(TARGET, true, undefined);
+    res  = await runExecProcessOnUrl(TARGET, undefined);
     expect(res.ok);
     expect(res.status).toBe(200);
     expect(res.headers.get("Content-Type")).toMatch(/json/);
-    assertType<Array<SaveStruct>>(res.body);
-    expect(res.body.length).toBe(3);
+    // assertType<Array<SaveStruct>>(res.body);
+    expect(res.body.length).toBeGreaterThan (3);
 
-    res: SimpleResponse = await runFetch(TARGET, true, { method: "post", body: transform2SaveStruct(fixture3()) });
+    res  = await runExecProcessOnUrl(TARGET, { 
+      method: "post", 
+      body: JSON.stringify(transform2SaveStruct(fixture3())) ,
+      headers:{'Content-Type': 'application/json; charset=utf-8', 'Accept':'application/json; charset=utf-8' } 
+    });
     expect(res.ok);
     expect(res.status).toBe(201);
     expect(res.headers.get("Content-Type")).toMatch(/json/);
     try {
-      if (typeof res.body === "string") {
+      if (typeof res.body != "string") {
         throw Error("Expected JSON object");
       } else {
         let obj = JSON.parse(new String(res.body).trim());
         expect(obj.statusCode).toBe(204);
       }
     } catch (e: unknown) {
-      console.log("Test quit early saying " + (e as Error).message);
+      console.log("[SECOND] Test quit early saying " + (e as Error).message);
       expect(false).toBe(true);
     }
 
-    ret: SimpleResponse = runFetch(TARGET, true, undefined);
+    res = await runExecProcessOnUrl(TARGET, undefined);
     expect(res.ok);
     expect(res.status).toBe(200);
     expect(res.headers.get("Content-Type")).toMatch(/json/);
     assertType<Array<SaveStruct>>(res.body);
-    expect(res.body.length).toBe(4);
+    expect(res.body.length).toBeGreaterThan (4);
   });
+
+  // IOIO XXX add a wonky object to POST, to show error state is sensible
 
   it("vitest of skip DEMO TEST 1111", { skip: true }, async (): Promise<boolean> => {
     return new Promise(async (a, b) => {
