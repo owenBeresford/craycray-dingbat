b02412e7d350156bb07f921c543b65d112847f32
oab1 <oab1@users.sourceforge.net>
2026-05-08 15:59:46 +0100

    Move client API tests to use cURL, as Node doesn't support HTTP2 + TLS yet ??git status

--- common/util.ts
+++ common/util.ts
@@ -1,4 +1,5 @@
 import type { SaveStruct } from './types/SaveStruct';
+import {  toHex as monkeyPath_toHex } from 'es-arraybuffer-base64/Uint8Array.prototype.toHex';
 
 /**
  * isMobile
@@ -115,8 +116,9 @@ export function clearSelection(): void {
 if(!( globalThis.crypto || globalThis.crypto.subtle) ) {
   throw new Error("JS runtime doesn't allow access to crypt/ hash functions.  Cannot proceed");
 }
-if(!Uint8Array.prototype.toHex) {
-  throw new Error("JS runtime isn't allowing access to baseline features");
+
+if(!('toHex' in Uint8Array.prototype)) {
+  Uint8Array.prototype.toHex=monkeyPath_toHex;
 }
 
 /**
@@ -124,12 +126,12 @@ if(!Uint8Array.prototype.toHex) {
  * Create a predictable hash of blob of data to support idempotency and duplicate collapse.
    // this is 1handed hashing, not cmp capacity
 
- * @param {Array<SaveStruct> } dat
- * @param {Readonly<string> ="SHA-256"} hash - the algorithm to apply, in 'spec naming notation'
+ * @param {Array<SaveStruct>} dat
+ * @param {Readonly<string>="SHA-256"} hash - the algorithm to apply, in 'spec naming notation'
  * @public
  * @returns {string} - hex encoded
  */
-export async function hashState(dat:Array<SaveStruct>, hash:Readonly<string>="SHA-256"):Promise<string> {
+export async function hashState(dat:Array<SaveStruct>, hash:Readonly<string>="SHA-256"):Promise<string>{
   let step1:string= JSON.stringify(dat);
       //step2:  this step should be obsolete/ irrelevant, but is critical failure if absent
   let step2 = new TextEncoder().encode(step1);
@@ -147,6 +149,130 @@ export interface SimpleResponse {
   ok: boolean;
   status:number;
 }
+
+export interface FileExecFlags {
+  cwd?:string|URL; 
+  env?:Object;
+  encoding?:strin ;
+  timeout?:number ; // ms
+  maxBuffer?:number ;
+  killSignal?:string|integer ;
+  uid?:number; 
+  gid?:number;
+  windowsHide?:boolean ;
+  windowsVerbatimArguments?:boolean ;
+  shell?:boolean|string|boolean ;
+  signal?:AbortSignal;
+};
+
+interface RunExecReturn {
+    reqt:Record<string, string>;
+    resp:Record<string, string>;
+};
+
+
+// https://nodejs.org/api/child_process.html#child_processexecfilefile-args-options-callback
+export async function runExecProcessOnUrl(
+    url: string,
+    extra:RequestInit | undefined
+        ):Promise<SimpleResponse> {
+  if (typeof process !== "object") {
+    return;  // a browser
+  }
+  var execFile:Function;
+  if(! execFile ) {
+    let tmp=await import('node:child_process');
+    execFile = tmp.execFile;
+  }
+ 
+
+  return new Promise(async (good: PromiseSucceed<SimpleResponse>, bad: PromiseReject): void => {
+// stderr has headers
+  // stdout has response body
+  const CB=(error:Error, stdout:string| Buffer, stderr: string|Buffer ):void => {
+    if(error) {
+      console.error("cURL failed:", error.message);
+      return bad(error);
+    }
+    let headers=parseHeaders(stderr);
+    let h2=new Headers();
+    for(let i in headers.resp ) {
+      h2.append( i, headers.resp[i] );
+    }
+
+    let exit:SimpleResponse= {
+        body: stdout.trim(),
+        headers: h2,
+        ok: Math.floor( parseInt(headers.resp['status'], 10) /100) === 2,
+        status: parseInt(headers.resp['status'], 10 ),
+      } as SimpleResponse;
+    return good(exit);
+  };
+
+  function parseHeaders(str:string):RunExecReturn {
+    let bits:Array<string>=str.split("\n");
+    let out={reqt:{}, resp:{} } as RunExecReturn;
+    
+    for(let i=0; i<bits.length; i++ ) {
+      switch(bits[i][0]) {
+      case '>': {
+        let annoying= parseHeader2(bits[i]);
+        out.reqt[annoying[0]]=annoying[1];
+        break;
+        }
+      case '<': {
+        let annoying= parseHeader2(bits[i]);
+        out.resp[annoying[0]]=annoying[1];
+        break;
+      }
+      default: break;
+    }
+    }
+    return out;
+  }
+
+  // > GET /api/shared-state HTTP/2
+  // < HTTP/2 200
+  function parseHeader2(str:string):Array<string> {
+    str=str.trim();
+    str=str.substring(1, str.length);
+    str=str.trim();
+    if( str.indexOf(':')===-1 ) {
+      if( str.indexOf("HTTP/")===0 ) {
+        return [ "status", str.substring( str.indexOf(' ')+1, str.length ) ];
+
+      } else {
+        return [ "method", str.substring(0, str.indexOf(' ') ) ];
+      }
+      
+    } else {
+      return [ 
+      str.substring(0, str.indexOf(':')).trim(), 
+      str.substring( str.indexOf(':')+2, str.length).trim(),
+      ];
+    }
+  }
+
+  let args:Array<string>=[
+      "-v", "-k", "-m1", url,  
+  ];
+  if(extra && ( "method" in extra)) {
+    args.push( "-X"+extra['method'].toUpperCase() );
+  }
+  if(extra &&( "body" in extra)) {
+    args.push( "-d");
+    args.push( extra['body'] );
+  }
+  if(extra && ("headers" in extra) ) {
+    for(let i in extra.headers ) {
+      args.push( "-H"+i+":"+extra.headers[i] );
+    }
+  }
+  const options:FileExecFlags = { windowsHide:true, shell:false };
+  execFile("/usr/bin/curl", args, options, CB);
+  });
+}
+
 
 /**
  * runFetch
---
+++ server-src/src/test/runCURL.vitest.mjs
@@ -0,0 +1,25 @@
+import { assert, describe, it, expect, assertType, beforeAll, afterAll } from "vitest";
+
+import { delay, runFetch, runExecProcessOnUrl} from "../../../common/util";
+ 
+describe("I can compile external script handling", () => {
+  const TARGET = "https://app.hiss:3001/api/shared-state";
+
+  it("can run an URL via cURL", async () => {
+    const res = await runExecProcessOnUrl(TARGET, undefined);
+    expect(res.ok);
+    expect(res.status).toBe(200);
+    expect(res.headers.get("Content-Type")).toMatch(/json/);
+    expect(res.body.length).greaterThan(1);
+  });
+
+  it("can run POST an URL via cURL", async () => {
+    const res = await runExecProcessOnUrl(TARGET, { method:"POST", body:"[]"} );
+    expect(res.ok);
+    expect(res.status).toBe(200);
+    expect(res.headers.get("Content-Type")).toMatch(/json/);
+    expect(res.body.length).greaterThan(1);
+  });
+
+
+});
