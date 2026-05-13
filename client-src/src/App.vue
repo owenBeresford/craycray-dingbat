003711bf904279fb0851b7f18dc92d50927eed18
oab1 <oab1@users.sourceforge.net>
2026-05-13 16:42:37 +0100

    Move current data fixture into App.vue, so can add dataOnLoad

--- client-src/src/App.vue
+++ client-src/src/App.vue
@@ -10,7 +10,19 @@ import { defineComponent } from "vue";
 
 // import { nextId } from "./services/util";
 import { TTL_FOR_HELP, DEFAULT_HELP_SHOW } from './Constants';
+import type { FactoryArtefact } from "./services/DataFactory";
+import type { ListCollection } from "./types/ListCollection";
+import { createDataFactory, ListData, setupCurrentList } from './services/DataFactory';
+import {fixture1 } from '../../common/fixture-lists';  // IOIO
+
 import TabBar from "./components/TabBar.vue";
+ 
+//IOIO  
+// const ListData2=setupCurrentList();
+const tmp:FactoryArtefact = createDataFactory(fixture1())
+
+// the fixture is alway present, no race condition possible
+ListData.updateData( tmp.currentData ?? {} as ListCollection );
  
 export default defineComponent({
   name: "ShoppingApp",
@@ -23,6 +35,7 @@ export default defineComponent({
     helpText: "menu",
     canSeeHelp: DEFAULT_HELP_SHOW,
     ttl: TTL_FOR_HELP,
+    dataOnLoad: tmp.currentData.count()>0
   },
   data() {
 // maybe lineup state-keys to show net status in latyer builds    
--- client-src/src/main.ts
+++ client-src/src/main.ts
@@ -6,23 +6,11 @@ import { STORE } from "./services/Store";
 import { APP_NAME, ROOT_NODE } from "./Constants";
 import { StaticRoutes } from "./components/Routing";
 import ShoppingApp from "./App.vue";
-import { createDataFactory, ListData, setupCurrentList } from './services/DataFactory';
-import {fixture1 } from '../../common/fixture-lists';  // IOIO
-import type { FactoryArtefact } from "./services/DataFactory";
-import type { ListCollection } from "./types/ListCollection";
 
 const TOOL = createApp(ShoppingApp, { currentStateKey:"scr1", instanceId:"v1.1" });
 TOOL.use(StaticRoutes);
 TOOL.use(Vue3TouchEvents as Plugin);
 TOOL.use(STORE);
-
-//IOIO  
-// const ListData2=setupCurrentList();
-const tmp= createDataFactory(fixture1())
-
-// the fixture is alway present, no race condition possible
-ListData.updateData( tmp.currentData ?? {} as ListCollection );
-console.log("In initial ShoppingApp main", ListData);
 
 STORE.install(TOOL, APP_NAME);
 TOOL.mount(ROOT_NODE);
