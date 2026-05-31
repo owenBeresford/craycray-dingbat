f73350214ad6b078e86ec819d1c34308c1512e03
oab1 <oab1@users.sourceforge.net>
2026-05-31 16:52:06 +0100

    Add modules to have logging on mobile

---
+++ client-src/src/components/MessageBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <details id="msgBar" class="msgBar" :key="currentStateKey" :data-testId="testId">
+    <summary>Log messages</summary>
+    <ul :data-testId="msgBodyId" clsss="scrollBlock">
+      <li><strong>Log messages that you can see on mobile</strong></li>
+      <!-- li><span class="button" :data-testId="refreshId" @click.prevent="refresh" @tap.prevent="refresh" @keypress.prevent="refresh">Refresh the log.</span></li -->
+      <li v-for="(i, j) in messages" :key="j" v-html="i"></li>
+    </ul>
+  </details>
+</template>
+
+<script lang="ts">
+// https://github.com/josueggh/a11y-cheatsheet
+import { defineComponent, inject, ref } from "vue";
+ 
+import type { GuessEvent } from "../../../common/types/infill-DOM-types-for-tests";
+import { useLog } from '../services/LogStack';
+import { useUIText } from "../services/Localisation";
+ 
+const TEXT = useUIText();
+
+export default defineComponent({
+  name: "MessageBar",
+  components: {},
+  props: {
+    currentStateKey: { type: String, default: "logging1" },
+    testId: { type: String, required: true },
+    enabled: { type: Boolean, default: false },
+    msgs: {
+      type: Object,
+      default: () => {
+        return useLog();
+      },
+    },
+  },
+  data() {
+    return {
+      msgBodyId: this.$props.testId + "Msgs1",
+      refreshId:this.$props.testId + "Refresh1",
+      
+    };
+  },
+  methods:{
+    refresh:function(e:GuessEvent):void {
+        console.log("Data pretend refresh" );
+    }
+  },
+  computed: {
+    // keying structure isnt complex, BUT logging shouldnt change, so keys shouldnt change
+    messages:function ():Array<string> {
+      if( this.msgs) { 
+        return this.msgs.readWhole().log;
+      } else {
+        return [];
+      }
+    },
+  },
+});
+</script>
+
+
+
---
+++ client-src/src/services/LogStack.ts
@@ -0,0 +1,101 @@
+import { reactive, readonly } from 'vue';
+
+import type { Loggable } from "../types/Loggable";
+import { MAX_LOG_LENGTH, LOGGING_ENABLED } from "../Constants";
+// add component flag for "no logging", to reduce RAM used if it isnt interesting
+
+/**
+ * useLog
+ * Boring use* function
+ 
+ * @public
+ * @returns {Loggable}
+ */
+export function useLog(): Loggable {
+  if(!SELF) { SELF=new LogService(); }
+  return SELF;
+}
+let SELF:Loggable;
+
+/**
+ * LogService
+ * A collection class for log events.
+   @todo in future add paging on read side
+   @todo actually add collection features, if I get a usecase
+   @todo maybe make some features be feature-flags if more time
+
+ * @public
+ */
+export class LogService implements Loggable {
+  protected log: Array<string>;
+
+
+  /**
+     * constructor
+     * A boring con'tor
+ 
+     * @public
+     * @returns {self}
+     */
+  public constructor() {
+    this.log=reactive({
+      log: [ "some text here" ] as string[]
+            });
+  }
+
+  // @TODO
+  addEvent(e: Event, volume: string): void { }
+
+   /**
+     * addRaw
+     * The "write log" function.  Alters local state, doesn't directly output anything
+     * If this requirement scales, make event objects like Stripe / ELK traces
+     *  I'm ignoring colour options for now, as logging doesn't need to be pretty, just timely
+     * WARN: after a data volume, the unshift() is probably slower that push() then reverse to render, or reverse via CSS
+ 
+     * @param {string} msg
+     * @param {string}  volume
+     * @public
+     * @returns {void}
+     */
+  public addRaw(msg: string, volume: string): void {
+    if(!LOGGING_ENABLED) { return; } // reduce RAM used
+
+    let d = new Date();
+    volume = volume.toUpperCase();
+    msg = `${volume} ${d.getUTCHours()}:${d.getUTCMinutes()}:${d.getUTCSeconds()} ${msg} `;
+    if (msg.length > MAX_LOG_LENGTH) {
+      msg = msg.substring(0, MAX_LOG_LENGTH);
+    }
+    console.debug(msg);
+
+    this.log.log.unshift(msg);
+    if (this.log.log.length % 100 === 0) {
+      this.log.log.unshift("NOTICE --:-- >>>>>>>>>>>>>>>>>> 100 row marker <<<<<<<<<<<<<<<<<<");
+    }
+  }
+
+  /**
+     * readWhole
+     * Data accessor, for whole data volume
+ 
+     * @public
+     * @returns {Readonly<Array<string>>}
+     */
+  public readWhole(): Readonly<Array<string>> {
+    return readonly(this.log);
+  }
+
+  /**
+     * readHead
+     * Data accessor, this includes data volume limit
+     * FIX FOR REACTIVITY
+ 
+     * @param {number} rows
+     * @public
+     * @returns {Array<string>}
+     */
+  public readHead(rows: number): Array<string> {
+    return this.log.slice(0, rows);
+  }
+}
