0a87ff960afef55a9f0704835b864036512d214d
oab1 <oab1@users.sourceforge.net>
2026-05-31 17:13:05 +0100

    Inject new logging feature

--- client-src/src/App.vue
+++ client-src/src/App.vue
@@ -2,14 +2,15 @@
   <div class="wholePage" :data-testid="instanceId" :key="currentStateKey">
     <TabBar currentStateKey="tabar1" :data-testid="tabId" />
     <router-view class="view" />
+    <MessageBar :msgs="log" :testId="msgId" :currentStateKey="msgState" :enabled="loggingEnabled" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
-// import { nextId } from "./services/util";
-import { TTL_FOR_HELP, DEFAULT_HELP_SHOW } from "./Constants";
+import { TTL_FOR_HELP, DEFAULT_HELP_SHOW, LOGGING_ENABLED } from "./Constants";
+import { useLog } from './services/LogStack';
 import type { FactoryArtefact } from "./services/DataFactory";
 import type { ListCollection } from "./types/ListCollection";
 import type { MainAppProps } from "./types/ComponentProps";
@@ -17,6 +18,7 @@ import { createDataFactory, ListData, setupCurrentList } from "./services/DataFa
 import { fixture1 } from "../../common/fixture-lists"; // IOIO
 
 import TabBar from "./components/TabBar.vue";
+import MessageBar from "./components/MessageBar.vue";
 
 //IOIO
 // const ListData2=setupCurrentList();
@@ -25,9 +27,11 @@ const tmp: FactoryArtefact = createDataFactory(fixture1());
 // the fixture is alway present, no race condition possible
 ListData.updateData(tmp.currentData ?? ({} as ListCollection<string>));
 
+const log=useLog();
+ 
 export default defineComponent({
   name: "ShoppingApp",
-  components: { TabBar },
+  components: { TabBar, MessageBar },
   props: {
     currentStateKey: { type: String, default: "root1" },
     instanceId: { type: String, required: true },
@@ -37,11 +41,17 @@ export default defineComponent({
     canSeeHelp: DEFAULT_HELP_SHOW,
     ttl: TTL_FOR_HELP,
     dataOnLoad: tmp.currentData.count() > 0,
+    log: log,
   },
   data() {
-    // maybe lineup state-keys to show net status in latyer builds
+    // IOIO XXX maybe lineup state-keys to show net status in later builds
+
     return {
       tabId: this.$props.instanceId + "TabBar1",
+      msgId: this.$props.instanceId + "Msg1",
+      msgState: this.$props.currentStateKey+"Msg1",
+      log:log,
+      loggingEnabled: LOGGING_ENABLED,
     };
   },
 });
