5255a25df19cf5b8e65ee51d101438f7bdb9b518
oab1 <oab1@users.sourceforge.net>
2026-06-14 15:24:09 +0100

    NOISE changes: Try to cause more effects from this file

--- client-src/src/test/vitest.setup.ts
+++ client-src/src/test/vitest.setup.ts
@@ -1,15 +1,28 @@
 // GLOBAL TOOLS
 // globalSetup: "./src/test/vitest.setup.ts",
 //
-/// <reference types="../../../common/types/env.d.ts" />
+// / <reference types="../../../common/types/env.d.ts" />
 
+/// <reference types="vite/client" />
+
+export declare const _LOGGING_: Readonly<boolean>;
+
+declare global {
+  interface Window {
+    readonly _LOGGING_: boolean;
+  }
+}
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 export async function setup(): Promise<void> {
+
+  console.warn("Vitest setup file is executed. ");
   Object.defineProperty(globalThis, "_LOGGING_", {
     value: process.env.NODE_ENV !== "production",
     writable: true,
   });
 }
-console.warn("Vitest setup file is excecuted ");
+console.warn("Vitest setup file is loaded ");
 export async function teardown(): Promise<void> {}
 
 // ON EACH TEST TOOLS
