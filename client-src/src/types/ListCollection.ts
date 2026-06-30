ef6365aa00ff0780cb756a28c8ab322c1457e682
oab1 <oab1@users.sourceforge.net>
2026-06-30 16:47:12 +0100

    Add some comments, as tyypes are now more complex

--- client-src/src/types/ListCollection.ts
+++ client-src/src/types/ListCollection.ts
@@ -1,7 +1,8 @@
 import type { TestDataSchema } from "../../../common/types/TestDataSchema";
 import { BaseList } from "../services/AList";
 
-// a set of listables, carefuly different method names to reduce kaos
+// A set of listables, 
+// Carefully different method names to reduce kaos
 export interface ListCollection<T> {
   create(nom: string): number;
   poll(): Promise<boolean>;
@@ -13,14 +14,16 @@ export interface ListCollection<T> {
   merge(next: ListCollection<T>): boolean;
   append(ret: InstanceListable<T>): boolean;
   searchItems(égaler: string | RegExp): Array<MatchedItems>;
+  listNames(): Array<string> ;
 
-  //  store(ret: StdList, offset: number): boolean;
   saveAllLists(): Promise<boolean>;
   loadAllLists(): boolean;
 }
-
-// attribute names in french, so there is no possible collision between JS keywords and them
-// other languages would also work
+ 
+// Attribute names in french, so there is no possible collision between JS keywords and them
+//    Other languages would also work
+// This is just the data to work with JSON better.
+// This is meta data, the list itself is in the next interface
 export interface ListStruct {
   nom: string;
   // we have the creation date, as sorting may be needed in future
@@ -33,10 +36,10 @@ export interface ListStruct {
   id: number;
 }
 
+// a singular list, with flexible data
 export interface InstanceListable<T> {
   éléments: Array<T>;
 
-  // instance functions
   add(nouveau: T): boolean;
   edit(offset: number, nouveau: T): boolean;
   remove(offset: number): boolean;
@@ -47,23 +50,28 @@ export interface InstanceListable<T> {
   view(): ListStruct;
 }
 
-// this is for static functions.  Interfaces in TS cannot use the keyword static
+// this is for **static** functions.   currently unused
+// Interfaces in TS cannot use the keyword static YET
 export interface ModuleListable<T> {
-  //  new(): InstanceListable<T>;
+  new(): InstanceListable<T>;
   importTest<T, U extends BaseList<T>>(this: { new (): U }, origine: TestDataSchema): U;
   manual<T, V extends BaseList<T>>(this: { new (): V }, nom: string, id: number): V;
 }
 
 // only implemented on StdList, and generates other flavours of Listable
+// these are static functions
 export interface ExtendedListable<T> extends InstanceListable<T> {
   filter(égaler: string | RegExp): Array<string>;
   importTest(origine: ExtendedListable<T>): ExtendedListable<T>;
 }
 
+// The search results type
 export interface MatchedItems {
   item: string;
   list: number;
   key?: string;
 }
 
+// Convienance util, Only used in tests
 export type WholeClass<T> = InstanceListable<T> & ListStruct;
+
