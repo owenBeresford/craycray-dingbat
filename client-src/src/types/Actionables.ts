e8ccbb1703678808d40fc9d4a615d337c63ffecf
oab1 <oab1@users.sourceforge.net>
2026-06-30 16:57:59 +0100

    Write notify callbck type, not c-n-p a line a random

--- client-src/src/types/Actionables.ts
+++ client-src/src/types/Actionables.ts
@@ -7,9 +7,10 @@ import type { SearchContext, ThisListContext, TabBarContext } from "./ComponentP
 type SaferFunctionType = (...args: any[]) => any;
 export type FakeThis<T> = { [K in keyof T]: Ref<T[K]> };
 
+/* types for callbacks */
 export type UserAction<I> = (e: GuessEvent, ctx: I) => boolean;
 export type CBType = (d1: string | null) => any;
-export type NotifyType = (d1: string | null) => any;
+export type NotifyType = (d1: number) => any;
 
 /**
   The basic type for the Actionables structure
@@ -18,6 +19,7 @@ export interface ExternalMethods<I> {
   mount(ctx: I, mapClass: ExternalMethods<I>): MethodOptions;
 }
 
+/* a group of meta types that are specific *and* just a fancy array */
 export type SearchCtx = FakeThis<SearchContext>;
 export type ThisListCtx = FakeThis<ThisListContext>;
 export type TabBarCtx = FakeThis<TabBarContext>;
