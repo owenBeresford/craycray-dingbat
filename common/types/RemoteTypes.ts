e20de5477d0e3a98136ab9ff89f13432470b5a48
oab1 <oab1@users.sourceforge.net>
2026-06-30 16:49:48 +0100

    Improve readability for network agent implementations; Add some comments to file

--- common/types/RemoteTypes.ts
+++ common/types/RemoteTypes.ts
@@ -1,5 +1,7 @@
 import type { SaveStruct } from "./SaveStruct";
 
+// Definition for a class to be able to pull and push data to the REST API.
+// This is is network centric
 export interface DistantStorable {
   saveState(dat: Array<SaveStruct>): Promise<boolean>;
   loadState(): Promise<Array<SaveStruct>>;
@@ -7,18 +9,38 @@ export interface DistantStorable {
   poll(): Promise<boolean>;
 }
 
-// see https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+/**
+	Interface for what params can be passed to fetch
+	I added agent for differing HTTP implementations #leSigh
+
+ 	@see https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+*/
 export interface RemoteConfig {
   url: string;
   timeout: number;
-  headers: Record<string, string>; //  'Content-Type': 'application/json'
-  mode: string; // no-cors, *cors, same-origin
+  headers: Record<string, string>; // eg 'Content-Type': 'application/json'
+  mode: string; // allowed values no-cors, *cors, same-origin  
   method: string;
   credentials: string;
   agent?:any;
 }
 
+/* 
+	Simplified values stored as a hash, see above link.
+*/
+export interface RSRemoteConfig {
+  timeout: number;
+  headers: Record<string, string>; //  'Content-Type': 'application/json'
+  mode: string; // no-cors, *cors, same-origin
+  method: string;
+  credentials: string;
+}
+
+/*
+	Structure used in HTTP responses from the designated Agent
+*/
 export interface APIResponseType {
   statusCode: string;
   result: string;
 }
+
