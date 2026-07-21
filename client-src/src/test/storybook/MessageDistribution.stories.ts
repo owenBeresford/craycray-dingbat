822938c18f2fa3a610c47f74c1a1da777aeb2a8d
oab1 <oab1@users.sourceforge.net>
2026-07-21 07:51:10 +0100

    improvements to test

--- client-src/src/test/storybook/MessageDistribution.stories.ts
+++ client-src/src/test/storybook/MessageDistribution.stories.ts
@@ -19,8 +19,9 @@ async function grp2() {
   expect(obj instanceof MessageDistribution).toBe(true);
   expect((obj as unknown as MessageDistribution).forkThread()).toBe(true);
 
+  let buf:Array<string> = obj.getErrors();
   const msg1 = {
-    name: "empty list",
+    name: "empty list cant save",
     created: new Date().getTime(),
     edited: new Date().getTime(),
     count: 1,
@@ -28,9 +29,10 @@ async function grp2() {
 
     list: [],
   } as SaveStruct;
-  expect(await obj.saveState([msg1])).toBe(true);
+   expect( await obj.saveState([msg1]) ).toBe(true);
+    expect(buf).toEqual( obj.getErrors() ); 
 
-  const msg2 = {
+      const msg2 = {
     name: "normal list",
     created: new Date().getTime(),
     edited: new Date().getTime(),
@@ -54,7 +56,7 @@ async function grp2() {
   } as SaveStruct;
   expect(await obj.saveState([msg3])).toBe(true);
 
-  /*     // i can't get this to compile, so skipped.
+  /**     // i can't get this to compile, so skipped.
   		const msg4= {
   name: "partial data (probably test condition only)",
   edited: (new Date()).getTime(),
@@ -94,14 +96,15 @@ async function grp5() {
   expect((obj as unknown as MessageDistribution).forkThread()).toBe(true);
 
   const msg1 = {
-    name: "empty list",
+    name: "empty list, k, not",
     created: new Date().getTime(),
     edited: new Date().getTime(),
     count: 1,
     id: 1,
 
-    list: [],
+    list: ["asda adasda asdad ", "brgdrgdbsdgd sb bsef ", "cwserfsf cjkihiklhj ", "dwerwer ", "etyiutyut eytuiyuiy"],
   } as SaveStruct;
+console.log("ERTERT ", msg1.name); 
   expect(await obj.saveState([msg1])).toBe(true);
 
   const msg2 = {
@@ -113,6 +116,7 @@ async function grp5() {
 
     list: ["asda adasda asdad ", "brgdrgdbsdgd sb bsef ", "cwserfsf cjkihiklhj ", "dwerwer ", "etyiutyut eytuiyuiy"],
   } as SaveStruct;
+console.log("ERTERT ", msg2.name);
   expect(await obj.saveState([msg2])).toBe(true);
 
   const msg3 = {
@@ -123,10 +127,11 @@ async function grp5() {
     id: 1,
 
     list: [
-      "Werw ertert ert earterte erte rte erte ertert ertete erte ertet erter tete ertert ertert ertert et ".repeat(50),
+      "Werw ertert ert earterte erte rte erte ertert ertete erte ertet erter tete ertert ertert ertert et ".repeat(5),
     ],
   } as SaveStruct;
-  expect(await obj.saveState([msg3])).toBe(true);
+console.log("ERTERT ", msg3);  
+  expect( await obj.saveState([msg3]) ).toBe(true);
 
   /*     // i can't get this to compile, to skipped.
       const msg4= {
@@ -184,7 +189,7 @@ export const runCommunication: StoryObj = {
 
         this.result += "<br /> ✔ All async tests passed";
       } catch (err: unknown) {
-        this.result += "<br /> ❌ Test failed: " + (err as Error).message;
+        this.result += "<br /> ❌ Test failed: " + (err as Error).message ;
       }
     },
   }),
