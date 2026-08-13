f6b926118b34287333b2f2119eca2b1ef045c08e
oab1 <oab1@users.sourceforge.net>
2026-08-13 10:54:21 +0100

    Add change network code; loadAllLists is async now; Reimpl some features for readability

--- client-src/src/services/NetworkedListService.ts
+++ client-src/src/services/NetworkedListService.ts
@@ -1,12 +1,11 @@
 import { StdList } from "./AList";
 import { ListService } from "./ListService";
 import { RemoteStorage } from "./RemoteStorage";
+import { useMsgDistrib, MessageDistribution } from './MessageDistribution';
 
 import type { SaveStruct } from "../../../common/types/SaveStruct";
 import type { LocalCopy } from "./LocalCopy";
-// import type { ListCollection } from "../types/ListCollection";
 import type { DistantStorable } from "../../../common/types/RemoteTypes";
-//import type { Listable, ListStruct } from "../types/ListCollection";
 import type { PromiseSucceed, PromiseReject } from "../../../common/types/promises";
 import type { AbstractSelfNameClass } from "../../../common/AbstractSelfNameClass";
 import type { NotifyType } from "../types/Actionables";
@@ -68,6 +67,11 @@ export class NetworkedListService extends ListService {
     }
   }
 
+  async flipConnection(loin: DistantStorable): void {
+    delete this.loin;  // I hope there are no hanging Refs to that object
+    this.loin = loin;
+  }
+
   /**
    * poll
    * Check the remote data-sources are active (Wifi, and API running)
@@ -108,8 +112,67 @@ export class NetworkedListService extends ListService {
       } as SaveStruct);
     }
     await this.local.saveState(valeur);
-    await this.remote.saveState(valeur);
+    if( await this.poll()) {
+      await this.remote.saveState(valeur);
+
+    } else {
+      let temp=useMsgDistrib();
+      this.flipConnection( temp );
+      temp.forkThread();
+      await this.remote.saveState(valeur);
+    }
     return true;
+  }
+
+
+
+  /**
+   * loadAllLists
+   * Request data from both remote sources, cache response in local states
+
+   * @public
+   * @returns {boolean}
+   */
+  public async loadAllLists(): Promise<boolean> {
+    let répondeur = true;
+    let dat=await this.local.loadState();
+    if (!dat) {
+      return false;
+    }
+
+    this.catalog = this.catalog.splice(0, Infinity);
+    console.debug("From local state, pulled " + dat.length + " items.");
+    this.mapper(dat);
+
+    let inner= async ():void=> {
+      try {
+        dat=await this.remote.loadState();
+       if (!dat) {
+         répondeur=false;
+         return; 
+       }
+    } catch(e:unknown ) {
+      console.debug("Network error, "+(e as Error).message);
+      répondeur=false;
+      return; 
+    }
+
+      this.catalog = this.catalog.splice(0, Infinity);
+      this.mapper(dat);
+      this.notify(this.catalog.length);
+      console.debug("From remote state, replacing " + dat.length + " items (only has valid 'safed'/saved data).")
+    };
+
+    if(await this.poll()) {
+      await inner();      
+    } else {
+      let temp=useMsgDistrib();
+      this.flipConnection( temp );
+      temp.forkThread();
+      await inner();
+     }
+
+    return répondeur;
   }
 
   /**
@@ -122,58 +185,22 @@ export class NetworkedListService extends ListService {
    * @returns {void}
    */
   private mapper(liste: Array<SaveStruct>): void {
+    let noms:Array<string>=[];
+    for(let i=0; i<this.catalog.length; i++) {
+      noms.push( this.catalog[i].nom);
+    }
+
     //  eslint-disable-next-line no-restricted-syntax, guard-for-in, no-for-in-array
     for (const i in liste) {
-      if (liste[i].name) {
-        const tt = StdList.manual<string, StdList>(liste[i].name, liste[i].id);
-        tt.créé = new Date(liste[i].created);
-        tt.modifié = new Date(liste[i].edited);
-        tt.énumérer = liste[i].count;
-        tt.éléments = [...liste[i].list];
+      if (liste[i].name && !noms.includes(liste[i].name)) {
+        const tt = StdList.importRemote(liste[i]);
         this.catalog.push(tt as StdList);
-      } else {
+      } else if (!liste[i].name)  {
         console.warn("Unpacked list [" + i + "] has no name; Que?");
+      } else {
+        this.catalog[ noms.indexOf(liste[i].name) ].éléments=[...liste[i].list ];
+        this.catalog[ noms.indexOf(liste[i].name) ].modifié=liste[i].modified;
       }
     }
-  }
-
-  /**
-   * loadAllLists
-   * Request data from both remote sources, cache response in local states
-
-   * @public
-   * @returns {boolean}
-   */
-  // might need to make this a Promise, if the then() callbacks dont block the function return.
-  // public async loadAllLists(): Promise<boolean> {
-  public loadAllLists(): boolean {
-    let répondeur = true;
-    this.local.loadState().then((dat: Array<SaveStruct>): void => {
-      if (!dat) {
-        répondeur = false;
-        return;
-      }
-
-      this.catalog = this.catalog.splice(0, Infinity);
-      console.debug("From local state, pulled " + dat.length + " items.");
-      this.mapper(dat);
-      return;
-    });
-
-    this.remote.loadState().then((dat: Array<SaveStruct>): void => {
-      if (!dat) {
-        répondeur = false;
-        return;
-      }
-
-      this.catalog = this.catalog.splice(0, Infinity);
-      this.mapper(dat);
-      this.notify(this.catalog.length);
-      console.debug("From remote state, replacing " + dat.length + " items (only has valid 'safed'/saved data).");
-      return;
-    });
-
-    // i hope this works.  JS has no spinlocks
-    return répondeur;
   }
 }
