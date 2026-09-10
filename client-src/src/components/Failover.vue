a350e801f92eb86c57708ce90e25b02a68dc6e4d
owenBeresford <owenBeresford@users.noreply.github.com>
2026-09-10 08:32:46 +0100

    As I am using id=0 as meaningfull, allow this value

--- client-src/src/components/Failover.vue
+++ client-src/src/components/Failover.vue
@@ -8,7 +8,7 @@
     </p>
 
     <p>Returning to the <router-link class="button" :to="linkAll"> catalogue screen</router-link> may have data.</p>
-    <p v-if="id">
+    <p v-if="id>-1">
       Alternatively you where previously on
       <router-link class="button" :to="singleListURL"> a list edit screen</router-link>
     </p>
@@ -35,10 +35,10 @@ export default defineComponent({
   data() {
     const itinéraire: RouteLocationNormalizedLoadedGeneric = useRoute();
     let id = 0;
-    if (this.$props.id) {
+    if (this.$props.id != undefined) {
       id = this.$props.id;
     } else if (itinéraire.name === "a-list") {
-      if (itinéraire.params.index >= 1) {
+      if (itinéraire.params.index >= 0) { // allow EMPTY_LIST
         id = extractId(itinéraire.params.index);
       }
     }
